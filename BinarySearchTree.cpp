571f14b087cdf3ebfc4477cb1f255f284aa98997
ardiapri <ardiapri882@gmail.com>
2025-06-24 11:12:21 +0700

    menambahkan int main

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -140,10 +140,69 @@ public:
             return;
 
 
-        cout << ptr->info << " "; // Tampilkan nilai node saat ini
+        
         postorder(ptr->leftchild); // Kunjungi subtree kiri
-       postorder(ptr->rightchild); // Kunjungi subtree kanan
+        postorder(ptr->rightchild); // Kunjungi subtree kanan
+        cout << ptr->info << " ";  // Tampilkan nilai node saat ini
     }
+    bool isEmpty() 
+    {
+        return root == nullptr; // Mengembalikan true jika pohon kosong, false jika tidak
+    }
+    
+
 
    
+};
+
+int main()
+{
+    BinarySearchTree elements;
+    while (true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5) : ";
+
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            elements.insert();
+            break;
+        }
+        case '2':
+        {
+            elements.inorder(elements.root);
+            break;
+        }
+        case '3':
+        {
+
+            elements.preorder(elements.root);
+            break;
+        }
+        case '4':
+        {
+
+            elements.postorder(elements.root);
+            break;
+        }
+        case '5':
+            return 0;
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
+        }
+    }
 };
